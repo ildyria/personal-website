fb01f46611a5c0041710df747ad1da1296b29b81
Benoit Viguier <b.viguier@cs.ru.nl>
2017-10-04 12:09:08 +0000

    cleaning up

--- update_website_qwleiqhlansdfoab.php
+++ update_website_qwleiqhlansdfoab.php
@@ -1,6 +1,6 @@
 <?php
 $output = shell_exec('git pull -v origin master 2>&1');
 echo "<pre>".$output."</pre>";
-$output = shell_exec('./build.py');
+$output = shell_exec('./build.py 2>&1');
 echo "<pre>".$output."</pre>";
 ?>
