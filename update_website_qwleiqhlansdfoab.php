7bb9a079993a47d8246e060a4fcfb1f9a2c52b31
Benoit Viguier <b.viguier@cs.ru.nl>
2017-10-04 12:06:59 +0000

    WIP

--- update_website_qwleiqhlansdfoab.php
+++ update_website_qwleiqhlansdfoab.php
@@ -1,6 +1,6 @@
 <?php
 $output = shell_exec('git pull -v origin master 2>&1');
 echo "<pre>".$output."</pre>";
-$output = shell_exec('make');
+$output = shell_exec('make 2>&1');
 echo "<pre>".$output."</pre>";
 ?>
